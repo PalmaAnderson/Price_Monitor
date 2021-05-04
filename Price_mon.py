ce6ce770005a5fc3e1d130bc41a840ac694b8df2
PalmaAnderson <palma.anderson1@gmail.com>
2021-05-04 14:53:10 -0300

    Implementa leitura de arquivo e conversão para float

--- Price_mon.py
+++ Price_mon.py
@@ -1,24 +1,60 @@
 from selenium import webdriver
-PATH="C:\Program Files\chromedriver90.exe"
+import re
+PATH = "C:\Program Files\chromedriver90.exe"
+driver = webdriver.Chrome(PATH)
 
-driver=webdriver.Chrome(PATH)
+def pricetofloat(price):
+    try:
+        price=price.replace(".", "")
+    except:
+        pass
+    
+    try:
+        price=price.replace(",", ".")
+    except:
+        pass
 
-driver.get("https://www.kabum.com.br/produto/99679/placa-de-video-zotac-nvidia-geforce-rtx-2060-twin-fan-6gb-gddr6-zt-t20600f-10m")
-print(driver.title)
+    try:
+        price=price.replace(" ", "")
+    except:
+        pass
 
-#preco=driver.find_element_by_class_name("preco_desconto")
-#print(preco.text)
-##
-driver.get("https://www.mercadolivre.com.br/placa-de-video-nvidia-galax-geforce-rtx-20-series-rtx-2060-26nrl7hpx7oc-6gb/p/MLB15971892")
-print(driver.title)
+    try:
+        price=price.replace("R$", "")
+    except:
+        pass
 
-preco=driver.find_element_by_class_name("price-tag-fraction")
-print(preco.text)
-##
-driver.get("https://www.terabyteshop.com.br/produto/10304/placa-de-video-galax-geforce-rtx-2060-1-click-oc-6gb-26nrl7hpx7oc-gddr6-pci-exps")
-print(driver.title)
+    
+    return float(price)
 
-preco=driver.find_element_by_id("valVista")
-print(preco.text)
 
-driver.quit
+#.replace(",", ".").replace(" ","").replace ("R$",""))
+def busca(site,seletor,nome,link):
+    driver.get(link)
+    try:
+        price=0
+        if seletor=="Class":
+            price=driver.find_element_by_class_name(nome).text
+        if seletor=="Id":
+            price = driver.find_element_by_id(nome).text
+    except:
+        price=0
+
+    return price
+
+if __name__ == '__main__':
+    file = open("Links.csv", "r")
+    param = file.read()
+    itens = param.split("\n")
+
+    for x in range(1, len(itens)-1):
+        item = itens[x].split(";")
+        site = str(item[0])
+        seletor = str(item[1])
+        nome = str(item[2])
+        link = str(item[3])
+
+        price=busca(site,seletor,nome,link)
+        price=pricetofloat(price)
+        print (">>>>>>>>" ,site,">>>>>>>",price)
+    driver.quit
